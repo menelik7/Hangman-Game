03e6dafaa6bb95a8855cb97256d9a82543bfa70a
Menelik Tefera <menelikworku@gmail.com>
2017-05-14 20:53:13 -0400

    a few functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,8 @@ var options = ["discover", "dream", "stretch", "risk", "explore", "dive", "dance
 var win = 0;
 var loss = 0;
 var chance = 9;
-var guess = [];
+var guessedLetter = [];
+var correctGuess = 0;
 
 
 var html = "<p>Cool Stuff</p>";
@@ -13,18 +14,18 @@ var html = "<p>Pending...</p>";
        document.querySelector("#game").innerHTML = html;
 
 var html = "<p>Welcome!</p>";
-       document.querySelector("#instructions").innerHTML = html;
+       document.querySelector("#intro").innerHTML = html;
 
 var html = "<p>Would you like to play a word game?  The objective is to uncover the hidden word by guessing the correct letters.  If you decide to play, refer to the Score box for important stats.<p>" +
            "<p>Press any key to start!</p>";
-       document.querySelector("#intro").innerHTML = html;
+       document.querySelector("#instructions").innerHTML = html;
 
 
 // This function is run whenever the user presses any key.
 document.onkeyup = function(event) {
         // Randomly chooses a choice from the options array. This is the Computer's guess.
         var word = options[Math.floor(Math.random() * options.length)];
-        // Records underscores for the letters in "word".
+        // Displays underscores for the letters in "word".
         var wordLength = word.length;
         var underscores = "";
         for(i=0; i<wordLength; i++) {
@@ -33,10 +34,10 @@ document.onkeyup = function(event) {
 
         // Update displays.
         var html = "<p></p>";
-              document.querySelector("#intro").innerHTML = html;
+              document.querySelector("#instructions").innerHTML = html;
 
         var html = "<p>Pick a letter!</p>";
-               document.querySelector("#instructions").innerHTML = html;
+               document.querySelector("#intro").innerHTML = html;
 
         var html = "<p><strong>wins: " + win + "</strong></p>" +
            "<p><strong>losses: " + loss + "</strong></p>" +
@@ -49,23 +50,51 @@ document.onkeyup = function(event) {
 
                     // This function is run whenever the user picks a letter.
                     document.onkeyup = function(event) {
-                    // Determines which key was pressed
-                    var letter = String.fromCharCode(event.keyCode).toLowerCase();
-                    if (guess.indexOf(letter) <0) {
-                    guess.push(letter.toUpperCase());
-                    }
-                    else {
-                      return;
-                    }
-                    // Displays the letters the user has guessed thus far.
-                    var html = "<p>Letters already guessed: </p>" +
-                                "<p><strong><span style='color:#ff0000'>" + guess + "</span><strong></p>";
-                            document.querySelector("#guess").innerHTML = html;
+                        // Determines which key was pressed
+                        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+                        var pos = word.indexOf(userGuess);
+                        var letter = word.charAt(pos);
+                        var pos2 = guessedLetter.indexOf(userGuess);
 
-                    var correct = word.indexOf(letter);
-                    var display = word.charAt(correct);
-                    var replace = underscores.replace(/_/g, display);
+                
+                        // Displays the letters the user has guessed thus far.
+                        var html = "<p>Letters already guessed: </p>" +
+                                    "<p><strong><span style='color:#ff0000'>" + guessedLetter + "</span><strong></p>";
+                                document.querySelector("#guess").innerHTML = html;
 
+
+                        function displayLetter(){
+                          if (pos >= 0) {
+                            var txt = underscores.replace(pos, letter);
+                            document.getElementById("word").innerHTML = txt;
+                                if (pos2 >= 0) {
+                                  alert("Letter already guessed.  Please pick another letter");
+                                }
+                                else {
+                                guessedLetter.push(userGuess);
+                                }
+                          }
+                          else {
+                            if (pos2 >= 0) {
+                              alert("This letter has already been guessed.  Please make another choice");
+                            }
+                            else {
+                            }
+                          }
+
+
+                        function correctGuessCount() {
+                          if (correctGuess == wordLength) {
+                            alert("Congratulations Smarty Pants! You win!!!");
+                            alert("Would you like to test your brain power again?");
+                          }
+                          else {
+                            document.getElementById("intro").innerHTML = ("Pick another letter");
+                          }
+                        }
                     }
+ return;
 }
+
+
 
