71d8d8fc5ba3719cfec04cd6e0362bc9c006e5a2
Menelik Tefera <menelikworku@gmail.com>
2017-05-15 12:22:37 -0400

    join function works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,8 @@ var options = ["discover", "dream", "stretch", "risk", "explore", "dive", "dance
 var win = 0;
 var loss = 0;
 var chance = 9;
+var underScores = [];
 var guessedLetter = [];
-var correctGuess = 0;
 
 
 var html = "<p>Cool Stuff</p>";
@@ -26,11 +26,10 @@ document.onkeyup = function(event) {
         // Randomly chooses a choice from the options array. This is the Computer's guess.
         var word = options[Math.floor(Math.random() * options.length)];
         // Displays underscores for the letters in "word".
-        var wordLength = word.length;
-        var underscores = "";
-        for(i=0; i<wordLength; i++) {
-            underscores = underscores + "_ "
+        for(i=0; i<word.length; i++) {
+            underScores[i] = "_ ";
         }
+        var correctGuess = word.length;
 
         // Update displays.
         var html = "<p></p>";
@@ -44,57 +43,44 @@ document.onkeyup = function(event) {
            "<p><strong>Chances remaining: " + chance + "</strong></p>";
                document.querySelector("#game").innerHTML = html;
 
-        var html = "<p>" + underscores + "</p>";
-               document.querySelector("#word").innerHTML = html;
+        var html = "<p>" + underScores + "</p>";
+                document.querySelector("#word").innerHTML = html;
 
 
                     // This function is run whenever the user picks a letter.
                     document.onkeyup = function(event) {
                         // Determines which key was pressed
                         var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-                        var pos = word.indexOf(userGuess);
-                        var letter = word.charAt(pos);
-                        var pos2 = guessedLetter.indexOf(userGuess);
 
-                
-                        // Displays the letters the user has guessed thus far.
+                        for (var a=0;a<word.length;a++){
+                          if (word[a] === userGuess){
+                            underScores[a] = userGuess;
+                            correctGuess--;
+                          }
+                        }
+
+                        while (correctGuess >= 0){
+                          document.querySelector("#word").innerHTML = underScores.join("");
+                          break;
+
+                        }
+
+
+
+
+
+
+
+
+
+
+                         // Displays the letters the user has guessed thus far.
                         var html = "<p>Letters already guessed: </p>" +
                                     "<p><strong><span style='color:#ff0000'>" + guessedLetter + "</span><strong></p>";
                                 document.querySelector("#guess").innerHTML = html;
 
-
-                        function displayLetter(){
-                          if (pos >= 0) {
-                            var txt = underscores.replace(pos, letter);
-                            document.getElementById("word").innerHTML = txt;
-                                if (pos2 >= 0) {
-                                  alert("Letter already guessed.  Please pick another letter");
-                                }
-                                else {
-                                guessedLetter.push(userGuess);
-                                }
-                          }
-                          else {
-                            if (pos2 >= 0) {
-                              alert("This letter has already been guessed.  Please make another choice");
-                            }
-                            else {
-                            }
-                          }
-
-
-                        function correctGuessCount() {
-                          if (correctGuess == wordLength) {
-                            alert("Congratulations Smarty Pants! You win!!!");
-                            alert("Would you like to test your brain power again?");
-                          }
-                          else {
-                            document.getElementById("intro").innerHTML = ("Pick another letter");
-                          }
-                        }
-                    }
- return;
 }
+};
 
 
 
