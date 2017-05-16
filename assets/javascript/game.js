927b50399ef8ef3daa5740bc79c9439f1d78a681
Menelik Tefera <menelikworku@gmail.com>
2017-05-16 05:48:52 -0400

    indentation!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,150 +7,143 @@ var guessedLetter = [];
 var pics = 6;
 var winSound = document.getElementById("sound");
 
-//Initial display.
+
 var html = "<p>Win to display new images</p>";
         document.querySelector("#place").innerHTML = html;
-
 var html = "<p>Pending...</p>";
         document.querySelector("#game").innerHTML = html;
-
 var html = "<p>Welcome!</p>";
         document.querySelector("#intro").innerHTML = html;
-
 var html = "<p>Would you like to play a word game?  The objective is to uncover the hidden word by guessing the correct letters.  If you decide to play, refer to the Score box for important stats.<p>" +
            "<p><strong><center><span style='color:#ff0000'>Click the button below to start!</span></center></strong></p>";
         document.querySelector("#instructions").innerHTML = html;
 
-        document.getElementById("reset").onclick = function() {myFunction()};
-        function myFunction(){
-              var word = options[Math.floor(Math.random() * options.length)];
-              var chance = 12;
-              var correctGuess = 0;
-              underScores = [];
-              guessedLetter = [];
-              var html = "<p>Letters already guessed: </p>" +
-                         "<p><strong><span style='color:#ff0000'>...</span><strong></p>";
-                      document.querySelector("#guess").innerHTML = html;
 
-              for(i=0; i<word.length; i++){
-                underScores[i] = "_ ";
-              }
 
-              var html = "<p></p>";
-                    document.querySelector("#instructions").innerHTML = html;
+document.getElementById("reset").onclick = function() {myFunction()};
+    function myFunction(){
+                  var word = options[Math.floor(Math.random() * options.length)];
+                  var chance = 12;
+                  var correctGuess = 0;
+                  underScores = [];
+                  guessedLetter = [];
 
-              var html = "<p>Pick a letter!</p>";
-                     document.querySelector("#intro").innerHTML = html;
+                  var html = "<p>Letters already guessed: </p>" +
+                  "<p><strong><span style='color:#ff0000'>...</span><strong></p>";
+                  document.querySelector("#guess").innerHTML = html;
 
-              var html = "<p><strong>wins: " + win + "</strong></p>" +
-                 "<p><strong>losses: " + loss + "</strong></p>" +
-                 "<p><strong>Guesses remaining: " + chance + "</strong></p>";
-                     document.querySelector("#game").innerHTML = html;
+                  for(i=0; i<word.length; i++){
+                    underScores[i] = "_";
+                  }
 
-              var html = "<p>" + underScores + "</p>";
+                  var html = "<p></p>";
+                      document.querySelector("#instructions").innerHTML = html;
+                  var html = "<p>Pick a letter!</p>";
+                      document.querySelector("#intro").innerHTML = html;
+                  var html = "<p><strong>wins: " + win + "</strong></p>" +
+                     "<p><strong>losses: " + loss + "</strong></p>" +
+                     "<p><strong>Guesses remaining: " + chance + "</strong></p>";
+                      document.querySelector("#game").innerHTML = html;
+                  var html = "<p>" + underScores + "</p>";
                       document.querySelector("#word").innerHTML = html;
-
                       document.querySelector("#reset").innerHTML = ("Restart");
 
 
                       document.onkeyup = function(event) {
-                          // Determines which key was pressed
                           var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-                          var pos = guessedLetter.indexOf(userGuess);
-                          var pos2 = underScores.indexOf(userGuess);
-                          var trk = tracker.indexOf(userGuess);
+                              var pos = guessedLetter.indexOf(userGuess);
+                                  var pos2 = underScores.indexOf(userGuess);
+                                  var trk = tracker.indexOf(userGuess);
 
-                          if (trk === -1){
-                            alert("Please make a valid selection");
-                            return;
-                          }
+                                      if (trk === -1){
+                                        alert("Please make a valid selection");
+                                        return;
+                                      }
 
-                          for (var a=0;a<word.length;a++){
-                            if ((word[a] === userGuess) && (pos2 === -1)){
-                              underScores[a] = userGuess;
-                              correctGuess++;
-                              winSound.play();
-                            }
-                          }
+                                          for (var a=0;a<word.length;a++){
+                                            if ((word[a] === userGuess) && (pos2 === -1)){
+                                              underScores[a] = userGuess;
+                                              correctGuess++;
+                                              winSound.play();
+                                            }
+                                          }
 
-                              if (correctGuess <= word.length){
-                                document.querySelector("#word").innerHTML = underScores.join("");
-                                  if (pos === -1){
-                                    guessedLetter.push(userGuess);
-                                    chance--;
-                                    var html = "<p>Letters already guessed: </p>" +
-                                               "<p><strong><span style='color:#ff0000'>" + guessedLetter + "</span><strong></p>";
-                                            document.querySelector("#guess").innerHTML = html;
-                                   
-                                    var html = "<p><strong>wins: " + win + "</strong></p>" +
-                                               "<p><strong>losses: " + loss + "</strong></p>" +
-                                               "<p><strong>Guesses remaining: " + chance + "</strong></p>" +
-                                               "<p><strong>Correct guesses: " + correctGuess + "</strong></p>" +
-                                               "<p><strong>Word length: " + word.length + "</strong></p>";
-                                            document.querySelector("#game").innerHTML = html;
-                                  }
-                                  else {
-                                    alert("This letter has already been picked.  PLease pick another letter!");
-                                  }
-                              }
+                                      if (correctGuess <= word.length){
+                                        document.querySelector("#word").innerHTML = underScores.join(" ");
+                                          if (pos === -1){
+                                                guessedLetter.push(userGuess);
+                                                   chance--;
+                                                     var html = "<p>Letters already guessed: </p>" +
+                                                       "<p><strong><span style='color:#ff0000'>" + guessedLetter + "</span><strong></p>";
+                                                       document.querySelector("#guess").innerHTML = html;
+                                           
+                                                       var html = "<p><strong>wins: " + win + "</strong></p>" +
+                                                       "<p><strong>losses: " + loss + "</strong></p>" +
+                                                       "<p><strong>Guesses remaining: " + chance + "</strong></p>" +
+                                                     "<p><strong>Correct guesses: " + correctGuess + "</strong></p>" +
+                                                   "<p><strong>Word length: " + word.length + "</strong></p>";
+                                                document.querySelector("#game").innerHTML = html;
+                                          }
+                                          else {
+                                            alert("This letter has already been picked.  PLease pick another letter!");
+                                          }
+                                      }
 
 
                               if (correctGuess === word.length) {
-                                win++;
-                                    var html = "<p>Letters already guessed: </p>" +
-                                              "<p><strong><span style='color:#ff0000'>" + guessedLetter + "</span><strong></p>";
-                                            document.querySelector("#guess").innerHTML = html;
+                                  win++;
+                                        var html = "<p>Letters already guessed: </p>" +
+                                         "<p><strong><span style='color:#ff0000'>" + guessedLetter + "</span><strong></p>";
+                                              document.querySelector("#guess").innerHTML = html;
+                                               var html = "<p><strong>wins: " + win + "</strong></p>" +
+                                                  "<p><strong>losses: " + loss + "</strong></p>" +
+                                                   "<p><strong>Guesses remaining: " + chance + "</strong></p>" +
+                                                    "<p><strong>Correct guesses: " + correctGuess + "</strong></p>" +
+                                                   "<p><strong>Word length: " + word.length + "</strong></p>";
+                                                    document.querySelector("#game").innerHTML = html;
+                                                    document.querySelector("#intro").innerHTML = ("Congratulations!  You win!")
+                                                    document.querySelector("#reset").innerHTML = ("Keep playing");
 
-                                    var html = "<p><strong>wins: " + win + "</strong></p>" +
-                                                 "<p><strong>losses: " + loss + "</strong></p>" +
-                                                 "<p><strong>Guesses remaining: " + chance + "</strong></p>" +
-                                                 "<p><strong>Correct guesses: " + correctGuess + "</strong></p>" +
-                                                 "<p><strong>Word length: " + word.length + "</strong></p>";
-                                            document.querySelector("#game").innerHTML = html;
-                                            document.querySelector("#intro").innerHTML = ("Congratulations!  You win!")
-                                            document.querySelector("#reset").innerHTML = ("Keep playing");
-
-                                            if (win === 1){
-                                            document.getElementById('pic').src = "assets/images/2.jpg";
-                                            }
-                                            else if (win === 2){
-                                            document.getElementById('pic').src = "assets/images/3.jpg";
-                                            }
-                                            else if (win === 3){
-                                            document.getElementById('pic').src = "assets/images/4.jpg";
-                                            }
-                                            else if (win === 4){
-                                            document.getElementById('pic').src = "assets/images/5.jpg";
-                                            }
+                                                    if (win === 1){
+                                                    document.getElementById('pic').src = "assets/images/2.jpg";
+                                                    }
+                                                    else if (win === 2){
+                                                    document.getElementById('pic').src = "assets/images/3.jpg";
+                                                    }
+                                                    else if (win === 3){
+                                                    document.getElementById('pic').src = "assets/images/4.jpg";
+                                                    }
+                                                else if (win === 4){
+                                                document.getElementById('pic').src = "assets/images/5.jpg";
+                                                }
                                             else if (win === 5){
                                             document.getElementById('pic').src = "assets/images/6.jpg";
                                             }
-                                            else if (win === 6){
-                                            document.getElementById('pic').src = "assets/images/7.jpg";
-                                            }
-                                            else if (win === 7){
-                                            document.getElementById('pic').src = "assets/images/8.jpg";
-                                            }
+                                        else if (win === 6){
+                                        document.getElementById('pic').src = "assets/images/7.jpg";
+                                        }
+                                    else if (win === 7){
+                                    document.getElementById('pic').src = "assets/images/8.jpg";
+                                    }
                               }
                            
 
-                              if ((chance <= 0) && (correctGuess < word.length)){
-                                loss++;
-                                            document.querySelector("#intro").innerHTML = ("Oops!  You ran out of guesses!");
-                                            document.querySelector("#word").innerHTML = ('The word was "' + word.toUpperCase() + '"');
-                                            document.querySelector("#reset").innerHTML = ("Try again");
-                                    var html = "<p>Letters already guessed: </p>" +
-                                              "<p><strong><span style='color:#ff0000'>" + guessedLetter + "</span><strong></p>";
-                                            document.querySelector("#guess").innerHTML = html;
-
-                                       
-                                    var html = "<p><strong>wins: " + win + "</strong></p>" +
-                                                 "<p><strong>losses: " + loss + "</strong></p>" +
-                                                 "<p><strong>Guesses remaining: " + chance + "</strong></p>" +
-                                                 "<p><strong>Correct guesses: " + correctGuess + "</strong></p>" +
-                                                 "<p><strong>Word length: " + word.length + "</strong></p>";
-                                            document.querySelector("#game").innerHTML = html;
-                              }   
+                          if ((chance <= 0) && (correctGuess < word.length)){
+                              loss++;
+                                  document.querySelector("#intro").innerHTML = ("Oops!  You ran out of guesses!");
+                                      document.querySelector("#word").innerHTML = ('The word was "' + word.toUpperCase() + '"');
+                                          document.querySelector("#reset").innerHTML = ("Try again");
+                                          var html = "<p>Letters already guessed: </p>" +
+                                          "<p><strong><span style='color:#ff0000'>" + guessedLetter + "</span><strong></p>";
+                                          document.querySelector("#guess").innerHTML = html;
+                                   
+                                          var html = "<p><strong>wins: " + win + "</strong></p>" +
+                                      "<p><strong>losses: " + loss + "</strong></p>" +
+                                    "<p><strong>Guesses remaining: " + chance + "</strong></p>" +
+                                  "<p><strong>Correct guesses: " + correctGuess + "</strong></p>" +
+                                "<p><strong>Word length: " + word.length + "</strong></p>";
+                              document.querySelector("#game").innerHTML = html;
+                          }   
                       }
                        
 }
