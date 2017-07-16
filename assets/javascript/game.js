2d7ba8f00935a40cae39673a617bac7e7e508f39
Menelik Tefera <menelikworku@gmail.com>
2017-07-16 12:37:38 -0400

    soem pics changed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,14 @@
 //Declare Global Variables.
-var options = ["discover", "dream", "stretch", "risk", "explore", "dive", "dance", "climb", "melt", "motor", "child", "thorn", "witch", "craft", "bankrupt", "hammer", "technique", "tennis", "java", "computer", "declare", "microsoft", "excel", "health", "building", "jump", "lion", "camaro", "maxima", "love", "patience", "program", "justice", "knee", "attitude", "fiesty", "kind", "karate", "football", "ikea", "horse", "tesla", "electric", "inform", "process", "algorithm", "function", "disable", "duplicate", "extract", "resize", "represent", "deny", "accept", "reject", "depose", "flight", "company", "tycoon", "empire", "wealth", "money", "greed", "happiness", "success", "mastery", "construct", "manager"];
+var options = ["discover", "dream", "stretch", "risk", "explore", "dive", "dance", "climb", "melt", "motor", "child", "thorn", "witch", "craft", "bankrupt", "hammer", "technique", "tennis", "java", "computer", "declare", "microsoft", "excel", "health", "building", "jump", "lion", "camaro", "maxima", "love", "patience", "program", "justice", "knee", "attitude", "fiesty", "kind", "karate", "football", "ikea", "horse", "tesla", "electric", "inform", "process", "algorithm", "function", "disable", "duplicate", "extract", "resize", "represent", "deny", "accept", "reject", "depose", "flight", "company", "tycoon", "empire", "wealth", "money", "greed", "happiness", "success", "mastery", "construct", "manager", "acres", "adult", "advice", "attempt", "border", "breeze", "brick", "calm", "canal", "cast", "chose", "claws", "coach", "contrast", "cookies", "customs", "damage", "deeply", "depth", "discussion", "doll", "donkey", "essential", "exchange", "exist", "facing", "film", "finest", "fireplace", "floating", "folks", "fort", "garage", "grab", "habit", "happily", "heading", "hunter", "image", "instant", "kids", "label", "lungs", "melted", "memory", "mill", "mission", "monkey", "mount", "Norway", "nuts", "official", "ourselves", "palace", "plates", "poetry", "policeman", "positive", "possibly", "practical", "pride", "promised", "recall", "require", "rhyme", "rocky", "rubbed", "rush", "sale", "satisfied", "scared", "selection", "shake", "shaking", "shallow", "shout", "silly", "simplest", "slight", "slip", "slope", "soap", "solar", "species", "spin", "stiff", "swung", "tales", "thumb", "tobacco", "toy", "trap", "treated", "tune", "vapor", "vessels", "wealth", "wolf", "zoo"];
+
 var tracker = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var win = 0;
 var loss = 0;
 var winSound = document.getElementById("sound");
 
 //Home screen display.
-var html = "<p>Would you like to play a word game?  The objective is to uncover the hidden word by guessing the correct letters.  If you decide to play, refer to the Score box for important stats.</p>" +
-           "<p><strong><center><span style='color:#4D9C2D'>Click the button below to start!</span></center></strong></p>";
+var html = "<p>Care to exercise your brain power?  The objective is to uncover the hidden word by guessing the correct letters using your keyboard.  If you decide to play, refer to the Score box for important stats.</p>" +
+           "<p><strong><center><span style='color:#4D9C2D'>Click the button below to start</span></center></strong></p>";
             document.querySelector("#instructions").innerHTML = html;
 
 
@@ -26,7 +27,7 @@ function myFunction(){
 
             //Generate underscores matching the number of letters in the random hidden word.
             for(i=0; i<word.length; i++){
-              underScores[i] = "_ ";
+              underScores[i] = " _ ";
             }
 
             //Update displays once the player initiates the game.
@@ -170,7 +171,7 @@ function myFunction(){
                                             document.querySelector("#guess").innerHTML = html;
 
                                             document.querySelector("#game").innerHTML = html;
-                                            document.querySelector("#intro").innerHTML = ("Oops!  You ran out of guesses!");
+                                            document.querySelector("#intro").innerHTML = ("<strong><span style='color:#ff0000'>Oops!  You ran out of guesses.</span></strong>");
                                             document.querySelector("#word").innerHTML = ('The word was "' + word + '"');
                                             document.querySelector("#reset").innerHTML = ("Try again");
                                      
